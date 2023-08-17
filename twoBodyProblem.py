26050fa8d27bfb97f661a8980b0f1fa1d1f75010
Aniket Kumar <141261307+aniketkumar2001@users.noreply.github.com>
2023-08-17 23:35:15 +0530

    final update twoBodyProblem.py

--- twoBodyProblem.py
+++ twoBodyProblem.py
@@ -1,9 +1,11 @@
-import math
+import numpy as np
 import pygame
 from sys import exit
 pygame.init()
 clock = pygame.time.Clock()
-SCREEN = pygame.display.set_mode((1550, 800))
+
+SCREEN = pygame.display.set_mode((1550, 800))          
+f_rate = 60
 
 p1 = pygame.Surface((10, 10), pygame.SRCALPHA)
 pygame.draw.circle(p1, (255, 255, 0), (5, 5), 5)
@@ -11,61 +13,66 @@ pygame.draw.circle(p1, (255, 255, 0), (5, 5), 5)
 p2 = pygame.Surface((4, 4), pygame.SRCALPHA)
 pygame.draw.circle(p2, (255, 255, 255), (2, 2), 2)
 
-p1_y = 400
-p2_y = p1_y
-frate = 60
+dt = 1/(f_rate)
+
 
 #############################################################################
 # Parameters
 
 # for 1st object (yellow obj)
-vy1 = 0                             # intitial speed in y            
-vx1 = 0                             # intitial speed in x 
+vy1 = 0.0                             # intitial speed in y            
+vx1 = 0.0                             # intitial speed in x 
 k1 = 500000                         # k = G*M
 
 # for 2nd object (white obj)    
-vy2 = -20                           # intitial speed in y           
-vx2 = 0                             # intitial speed in x                  
+vy2 = -20.0                           # intitial speed in y           
+vx2 = 0.0                             # intitial speed in x                  
 k2 = 0                              # k = G*M
 
-r = 150                             # half the initial separation 
-
+r = 300.0                             # initial separation 
 
 ###############################################################################
-g = lambda k, x, y : -k/((math.sqrt(x*x + y*y))**2)
-p1_x = 775-r 
-p2_x = 775+r
-dt = 1/(frate)
+
+
+obj0=[]
+obj0.append([np.array([750.0,400.0]),np.array([vx1,vy1])])                # object 1
+obj0.append([np.array([750.0+r,400.0]),np.array([vx2,vy2])])             # object 2
+k = np.array([k1,k2])                                                  # GM [obj1,obj2]
+
+
+def addG(obj,i):
+    j=0
+    while j<2:
+        if j==i:
+            j+=1
+            continue
+        
+        s=obj0[j][0]-obj[0]
+        sMod=np.linalg.norm(s)
+        acc=(k[j]*s)/(sMod**3)
+
+        obj[1]+=[acc[0]*dt,acc[1]*dt]
+        obj[0]+=obj[1]*dt
+
+        j+=1
+
+    return obj   
+
+
 while True :
-    SCREEN.fill((0,0,0))            # to refresh the screen, try commenting this line.
     for e in pygame.event.get():
         if (e.type == pygame.QUIT):
             pygame.quit()
             exit()
-    SCREEN.blit(p1,(p1_x,p1_y))
-    SCREEN.blit(p2,(p2_x,p2_y))
-    x2 = p2_x-p1_x
-    y2 = p2_y-p1_y
-    x1=-x2; y1=-y2
-    g2 = g(k1, x2, y2)
-    g1 = g(k2, x1, y1) 
-    if x2<0:
-        g2 = -g2
-    if x1<0:
-        g1 = -g1
-    theta1 = math.atan(y1/x1)
-    theta2 = math.atan(y2/x2)
-    ax1 = g1*math.cos(theta1)
-    ay1 = g1*math.sin(theta1)
-    ax2 = g2*math.cos(theta2)
-    ay2 = g2*math.sin(theta2)
-    vx1 += ax1*dt
-    vy1 += ay1*dt
-    vx2 += ax2*dt
-    vy2 += ay2*dt
-    p2_x += vx2*dt
-    p2_y += vy2*dt
-    p1_x += vx1*dt
-    p1_y += vy1*dt
+
+    SCREEN.fill((0,0,0))                    # to refresh the screen, try commenting this line.
+    SCREEN.blit(p1,obj0[0][0]) 
+    SCREEN.blit(p2,obj0[1][0]) 
+    
+    i=0
+    while i<2: 
+        obj0[i] = addG(obj0[i],i)
+        i+=1
+
     pygame.display.update()
-    clock.tick(frate)
+    clock.tick(f_rate)
